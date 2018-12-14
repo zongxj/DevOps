a688ee28b1fa64e94eb77abe1e9bc529efb53521
zongxj <zongxj_cn@foxmail.com>
2018-12-14 23:09:52 +0800

    #CMDB 后台系统

--- CMDB/admin.py
+++ CMDB/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
-
-class ChoiceAdmin(admin.ModelAdmin):
-    fields = ['choice_text', 'votes', 'question']
-
-admin.site.register(Choice, ChoiceAdmin)
